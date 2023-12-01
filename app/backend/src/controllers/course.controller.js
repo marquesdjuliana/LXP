8d562d6de46b7970b52c04b029e0400a1bb02d38
Juliana Marques <marquesdjuliana@gmail.com>
2023-12-01 08:56:18 -0300

    Implements paged GET /courses

--- app/backend/src/controllers/course.controller.js
+++ app/backend/src/controllers/course.controller.js
@@ -1,13 +1,14 @@
 const { courseService } = require('../services');
 const mapStatusHTTP = require('../utils/mapStatusHTTP');
  
-const listAllCourses = async (_req, res) => {
-  const { status, data } = await courseService.listAllCourses();
+
+const listAllCourses = async (req, res) => {
+  const { page, pageSize } = req.query;
+
+  const { status, data } = await courseService.listAllCourses(page, pageSize);
   return res.status(mapStatusHTTP(status)).json(data);
 };
 
-
-
 module.exports = {
-  listAllCourses,
+  listAllCourses,  
 }
--- app/backend/src/services/course.service.js
+++ app/backend/src/services/course.service.js
@@ -1,7 +1,11 @@
 const { Course } = require('../models');
 
-const listAllCourses = async () => {
-  const courses = await Course.findAll();
+const listAllCourses = async (page = 1, pageSize = 10) => {
+  const offset = (page - 1) * pageSize;
+  const courses = await Course.findAll({
+    offset,
+    limit: parseInt(pageSize),
+  });
   return { status: 'SUCCESSFUL', data: courses };
 };
 
