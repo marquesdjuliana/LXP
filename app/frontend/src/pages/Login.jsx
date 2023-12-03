fec5c3d0c8cc22375bcc28287cb170433784a07f
Juliana Marques <marquesdjuliana@gmail.com>
2023-12-03 18:06:19 -0300

    Creates modal

--- app/frontend/src/pages/Login.jsx
+++ app/frontend/src/pages/Login.jsx
@@ -1,6 +1,69 @@
+// import React, { useState } from "react";
+// import { requestLogin, setToken, requestRole } from "../services/requests";
+// import { useNavigate } from "react-router-dom";
+// import "../styles/pages/login.css";
+
+// const Login = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [failedTryLogin, setFailedTryLogin] = useState(false);
+//   const navigate = useNavigate();
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     try {
+//       const { token } = await requestLogin("/login", { email, password });
+//       setToken(token);
+
+//       const { role: userRole } = await requestRole("/login/role");
+//       localStorage.setItem("token", token);
+//       localStorage.setItem("role", userRole);
+
+//       if (userRole === "student") {
+//         navigate("/courses");
+//       } else if (userRole === "professor") {
+//         navigate("/courses-professor");
+//       }
+//     } catch (error) {
+//       setFailedTryLogin(true);
+//     }
+//   };
+
+//   return (
+//     <div className="login-container">
+//       <form className="login-content" onSubmit={handleSubmit}>
+//         <input
+//           className="login-input"
+//           type="text"
+//           placeholder="Digite seu email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//         />
+//         <input
+//           className="login-input"
+//           type="password"
+//           placeholder="Senha"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+//         <button type="submit" className="login-button">
+//           Entrar
+//         </button>
+//       </form>
+//       {failedTryLogin && (
+//         <p className="error-message">
+//           O endereço de e-mail ou a senha não estão corretos. Por favor, tente novamente.
+//         </p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Login;
 import React, { useState } from "react";
 import { requestLogin, setToken, requestRole } from "../services/requests";
 import { useNavigate } from "react-router-dom";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@mui/material";
 import "../styles/pages/login.css";
 
 const Login = () => {
@@ -9,8 +72,14 @@ const Login = () => {
   const [failedTryLogin, setFailedTryLogin] = useState(false);
   const navigate = useNavigate();
 
+  const [openModal, setOpenModal] = useState(false); // Estado para controlar a exibição da modal
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
   const handleSubmit = async (event) => {
-    event.preventDefault(); // Evita o comportamento padrão de recarregar a página
+    event.preventDefault();
 
     try {
       const { token } = await requestLogin("/login", { email, password });
@@ -27,6 +96,7 @@ const Login = () => {
       }
     } catch (error) {
       setFailedTryLogin(true);
+      setOpenModal(true);
     }
   };
 
@@ -51,11 +121,15 @@ const Login = () => {
           Entrar
         </button>
       </form>
-      {failedTryLogin && (
-        <p className="error-message">
-          O endereço de e-mail ou a senha não estão corretos. Por favor, tente novamente.
-        </p>
-      )}
+      <Dialog open={openModal} onClose={handleCloseModal}>
+        <DialogTitle>Erro de Login</DialogTitle>
+        <DialogContent>
+          <p>O endereço de e-mail ou a senha não estão corretos. Por favor, tente novamente.</p>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseModal}>Fechar</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
