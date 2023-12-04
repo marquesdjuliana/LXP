6aa2389c9455e0943846bd38285019eb5309561c
Juliana Marques <marquesdjuliana@gmail.com>
2023-12-04 14:45:56 -0300

    Update images

--- app/frontend/src/pages/CoursesProfessor.jsx
+++ app/frontend/src/pages/CoursesProfessor.jsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { requestCoursesForProfessor } from '../services/requests';
-import { Container, Box, Typography, Paper, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
-import backgroundImages from '../images/lxpa.jpeg';
+import React, { useEffect, useState } from "react";
+import { requestCoursesForProfessor } from "../services/requests";
+import { Container, Box, Typography, Paper } from "@mui/material";
+import { Link } from "react-router-dom";
+import backgroundImages from "../images/lxp-professor.jpeg";
 
 const CoursesProfessor = () => {
   const [courses, setCourses] = useState([]);
 
   const getUserId = () => {
-    const userId = localStorage.getItem('userId');
+    const userId = localStorage.getItem("userId");
     return userId;
   };
 
   const getCoursesData = async () => {
     try {
-      const professorId = getUserId(); 
+      const professorId = getUserId();
       const response = await requestCoursesForProfessor(professorId);
       setCourses(response);
     } catch (error) {
@@ -26,30 +26,55 @@ const CoursesProfessor = () => {
     getCoursesData();
   }, []);
 
+
+
   return (
-    <Container maxWidth="md" style={{ marginTop: 300 }}>
-      <Paper elevation={3} style={{ backgroundColor: '#ADD8E6', padding: 40 }}>
-        <Box mt={8} textAlign="center">
-          <Typography variant="h4" component="h2" gutterBottom>
-            Courses for Professor
-          </Typography>
-          <ul style={{ listStyleType: 'none', padding: 0 }}>
-            {courses.map((course) => (
-              <li key={course.id} style={{ marginBottom: 20 }}>
-                <Paper elevation={1} style={{ backgroundColor: 'white', padding: 10 }}>
-                  <Link to={`/course-details/${course.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <Typography variant="h5" gutterBottom>
-                      Title: {course.title}
-                    </Typography>
-                  </Link>
-                  <Typography variant="body1">Duration: {course.duration}</Typography>
-                </Paper>
-              </li>
-            ))}
-          </ul>
-        </Box>
-      </Paper>
-    </Container>
+    <div
+      style={{
+        backgroundImage: `url(${backgroundImages})`,
+        backgroundSize: "cover",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+        <Container maxWidth="md">
+          <Paper
+            elevation={3}
+            style={{ backgroundColor: "rgba(135, 206, 250, 0.5)", padding: 20 }}
+
+          >
+            <Box mt={8} textAlign="center">
+              <ul
+                style={{ listStyleType: "none", padding: 0, textAlign: "left" }}
+              >
+                {courses.map((course) => (
+                  <li key={course.id} style={{ marginBottom: 20 }}>
+                    <Paper elevation={1} style={{ padding: 10 }}>
+                      <Link
+                        to={`/course-details/${course.id}`}
+                        style={{ textDecoration: "none", color: "inherit" }}
+                      >
+                        <Typography
+                          variant="h5"
+                          gutterBottom
+                          style={{ fontSize: "1.6rem" }}
+                        >
+                          Curso: {course.title}
+                        </Typography>
+                      </Link>
+                      <Typography variant="body1">
+                        Carga horária: {course.duration}
+                      </Typography>
+                    </Paper>
+                  </li>
+                ))}
+              </ul>
+            </Box>
+          </Paper>
+        </Container>
+    </div>
   );
 };
 
